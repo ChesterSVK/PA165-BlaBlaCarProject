e6c8e3b9106095a179455c0d67b104e297cb44da
Miroslav Laco <miroslav.laco@gmail.com>
2017-11-11 14:04:23 +0100

    PlaceService, PlaceServiceImpl added (with KISS methods).

--- blablacar-service/src/main/java/cz/fi/muni/pa165/teamred/service/PlaceService.java
+++ blablacar-service/src/main/java/cz/fi/muni/pa165/teamred/service/PlaceService.java
@@ -1,8 +1,65 @@
 package cz.fi.muni.pa165.teamred.service;
 
+import cz.fi.muni.pa165.teamred.entity.Place;
 import org.springframework.stereotype.Service;
 
-// todo
+import java.util.List;
+
+/**
+ * Service class operating over {@link cz.fi.muni.pa165.teamred.entity.Place} entity.
+ *
+ * @author miroslav.laco@gmail.com
+ */
 @Service
 public interface PlaceService {
+
+    /**
+     * Creates {@link cz.fi.muni.pa165.teamred.entity.Place} in app.
+     *
+     * @param place {@link cz.fi.muni.pa165.teamred.entity.Place} to be created
+     * @return created place
+     * @throws IllegalArgumentException if place param is null
+     */
+    Place createPlace(Place place) throws IllegalArgumentException;
+
+    /**
+     * Updates {@link cz.fi.muni.pa165.teamred.entity.Place} in app.
+     *
+     * @param place {@link cz.fi.muni.pa165.teamred.entity.Place} to be updated
+     * @throws IllegalArgumentException if place param is null
+     */
+    void updatePlace(Place place) throws IllegalArgumentException;
+
+    /**
+     * Removes {@link cz.fi.muni.pa165.teamred.entity.Place} in app.
+     *
+     * @param place {@link cz.fi.muni.pa165.teamred.entity.Place} to be removed
+     * @throws IllegalArgumentException if place param is null
+     */
+    void removePlace(Place place) throws IllegalArgumentException;
+
+    /**
+     * Finds {@link cz.fi.muni.pa165.teamred.entity.Place} in app by unique id.
+     *
+     * @param id id of {@link cz.fi.muni.pa165.teamred.entity.Place} to be found
+     * @return unique {@link cz.fi.muni.pa165.teamred.entity.Place} with provided id, null if not found
+     * @throws IllegalArgumentException if id param is null
+     */
+    Place findById(Long id) throws IllegalArgumentException;
+
+    /**
+     * Finds {@link cz.fi.muni.pa165.teamred.entity.Place} in app by unique name.
+     *
+     * @param name name of {@link cz.fi.muni.pa165.teamred.entity.Place} to be found
+     * @return unique {@link cz.fi.muni.pa165.teamred.entity.Place} with provided name, null if not found
+     * @throws IllegalArgumentException if name param is null
+     */
+    Place findByName(String name) throws IllegalArgumentException;
+
+    /**
+     * Finds all {@link cz.fi.muni.pa165.teamred.entity.Place} in app.
+     *
+     * @return all {@link cz.fi.muni.pa165.teamred.entity.Place} objects stored
+     */
+    List<Place> findAll();
 }
--- blablacar-service/src/main/java/cz/fi/muni/pa165/teamred/service/PlaceServiceImpl.java
+++ blablacar-service/src/main/java/cz/fi/muni/pa165/teamred/service/PlaceServiceImpl.java
@@ -1,8 +1,63 @@
 package cz.fi.muni.pa165.teamred.service;
 
+import cz.fi.muni.pa165.teamred.dao.PlaceDao;
+import cz.fi.muni.pa165.teamred.entity.Place;
 import org.springframework.stereotype.Service;
 
-// todo
+import javax.inject.Inject;
+import java.util.List;
+
+/**
+ * @author miroslav.laco@gmail.com
+ */
 @Service
 public class PlaceServiceImpl implements PlaceService {
+    @Inject
+    private PlaceDao placeDao;
+
+    @Override
+    public Place createPlace(Place place) throws IllegalArgumentException {
+        if(place == null) {
+            throw new IllegalArgumentException("Place to create is null!");
+        }
+        placeDao.create(place);
+        return place;
+    }
+
+    @Override
+    public void updatePlace(Place place) throws IllegalArgumentException {
+        if(place == null) {
+            throw new IllegalArgumentException("Place to update is null!");
+        }
+        placeDao.update(place);
+    }
+
+    @Override
+    public void removePlace(Place place) throws IllegalArgumentException {
+        if(place == null) {
+            throw new IllegalArgumentException("Place to remove is null!");
+        }
+        placeDao.delete(place);
+    }
+
+    @Override
+    public Place findById(Long id) throws IllegalArgumentException {
+        if(id == null) {
+            throw new IllegalArgumentException("Place id is null!");
+        }
+        return placeDao.findById(id);
+    }
+
+    @Override
+    public Place findByName(String name) throws IllegalArgumentException {
+        if(name == null) {
+            throw new IllegalArgumentException("Place name is null!");
+        }
+        return placeDao.findByName(name);
+    }
+
+    @Override
+    public List<Place> findAll() {
+        return placeDao.findAll();
+    }
 }
